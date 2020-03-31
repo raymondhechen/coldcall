4a03e84ef64770b50abff581cf0eaf5f37b37bdf
raymondhechen <rc284@duke.edu>
2020-03-30 22:02:14 -0400

    landing body updates

--- frontend/src/landing/landing.jsx
+++ frontend/src/landing/landing.jsx
@@ -4,6 +4,7 @@ import styled from 'styled-components';
 import ProfileIcon from './profile.svg';
 import Arrow from './arrow';
 import LandingDesign from './landingdesign.svg';
+import Duke from './duke.jpg';
 
 const Nav = styled.div`
     position: fixed;
@@ -15,7 +16,7 @@ const Nav = styled.div`
 
     display: flex;
     flex-direction: row;
-    justify-content: space-between
+    justify-content: space-between;
 `;
 
 const Logo = styled.div`
@@ -68,8 +69,8 @@ const RightWrapper = styled.div`
 `;
 
 const RightText = styled.div`
-    font-family: proxima-nova;
-    font-weight: bold;
+    font-family: lato;
+    font-weight: 800;
     font-size: 35px;
     line-height: 50px;
     position: relative;
@@ -100,10 +101,97 @@ const Picture = styled.img`
     height: auto;
 `;
 
-
 const Body2 = styled.div`
-    height: 100vh;
+    height: 90vh;
+    padding-top: 15vh;
+    display: flex;
+    flex-direction: row;
+    justify-content: space-between;
 `;
+
+const BodyLeft = styled.div`
+    width: 50%;
+`;
+
+const BodyRight = styled.div`
+    width: 50%;
+    margin: 7.5vh 5vw 0 0;
+`;
+
+const DukeImg = styled.img`
+    width: 40vw;
+    height: auto;
+    padding: 0 0 0 10vw;
+`;
+
+const BodyText1 = styled.div`
+    font-family: lato;
+    font-weight: 800;
+    font-size: 40px;
+    line-height: 50px;
+    position: relative;
+    padding: 0 0 0 10vw; 
+`
+
+const BodyText2 = styled.div`
+    font-family: proxima-nova;
+    font-weight: 500;
+    font-size: calc(1em + 1vw);
+    line-height: 50px;
+    position: relative;
+    padding: 5vh 0 0 15vw; 
+    line-height: 4.5vw;
+`;
+
+const BodyText3 = styled.div`
+    font-family: lato;
+    font-weight: 800;
+    font-size: 30px;
+    line-height: 50px;
+    position: relative;
+    padding: 5vh 0 0 0;
+    text-align: center;
+`;
+
+const SubFooter1 = styled.div`
+    background-color: #56CCF2;
+    height: 5vh;
+`;
+
+const SubFooter2 = styled.div`
+    background-color: #2D9CDB;
+    height: 5vh;
+`;
+
+const SubFooter3 = styled.div`
+    background-color: #2F80ED;
+    height: 5vh;
+`;
+
+const Footer = styled.div`
+    height: 20vh;
+    background-color: #4F4F4F;
+    display: flex;
+    flex-direction: row;
+    justify-content: space-between;
+`
+
+const FooterLeft = styled.div`
+    color: #E0E0E0;
+    font-family: Lato;
+    font-weight: 500;
+    font-size: 20px;
+    padding: 10vh 0 0 5vw;
+    width: 125vw;
+`
+
+const FooterRight = styled.div`
+    color: #E0E0E0;
+    font-family: Lato;
+    font-weight: 500;
+    font-size: 15px;
+    padding: 5vh 5vw 0 0;
+`
 
 
 class Landing extends Component {
@@ -141,8 +229,39 @@ class Landing extends Component {
 
                 <Arrow/>
 
-                <Body2/>
-            
+                <Body2>
+                    <BodyLeft>
+                        <BodyText1>
+                            Here's how it works:
+                        </BodyText1>
+                        <BodyText2>
+                            1.  Sign up! <br/>
+                            2.  Create your profile <br/>
+                            3.  Find other students <br/>
+                            4.  Organize a meeting <br/>
+                            5.  Get together!
+                        </BodyText2>
+                    </BodyLeft>
+                    <BodyRight>
+                        <DukeImg src={Duke} alt="duke"/>
+                        <BodyText3>
+                            We're at Duke!
+                        </BodyText3>
+                    </BodyRight>
+                </Body2>
+                
+                <SubFooter1/>
+                <SubFooter2/>
+                <SubFooter3/>
+                <Footer>
+                    <FooterLeft>
+                        © Cold Call 2020
+                    </FooterLeft>
+                    <FooterRight>
+                        Designed and Built with ❤️ by <br/>
+                        Raymond Chen, Shrey Gupta, Sachin Jainshankar, Jeffrey Luo, Luke Qin
+                    </FooterRight>
+                </Footer>
             </div>
         )
     }
